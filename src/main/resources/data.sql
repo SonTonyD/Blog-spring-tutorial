ecedcde18a699ba7961224e491605051452bdc2f
Son-tony DINH <sodin@P-LYOURFE>
2023-04-16 22:04:00 +0200

    remove insert id because Spring already handle this. If you specify the ID, it will generate conflicts when you need to insert data into database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO article (id, name, content, author) VALUES (1, 'article name', 'I am an article and this is the content', 'John');
-INSERT INTO article (id, name, content, author) VALUES (2, 'car article', 'I am an article and this is the content', 'John');
-INSERT INTO article (id, name, content, author) VALUES (3, 'plane article', 'I am an article and this is the content', 'John');
-INSERT INTO article (id, name, content, author) VALUES (4, 'sport article', 'I am an article and this is the content', 'Bob');
-INSERT INTO article (id, name, content, author) VALUES (5, 'news article', 'I am an article and this is the content', 'Bob');
-INSERT INTO article (id, name, content, author) VALUES (6, 'people article', 'I am an article and this is the content', 'Alice');
-INSERT INTO article (id, name, content, author) VALUES (7, 'science article', 'I am an article and this is the content', 'Alice');
+INSERT INTO article (name, content, author) VALUES ('article name', 'I am an article and this is the content', 'John');
+INSERT INTO article (name, content, author) VALUES ('car article', 'I am an article and this is the content', 'John');
+INSERT INTO article (name, content, author) VALUES ('plane article', 'I am an article and this is the content', 'John');
+INSERT INTO article (name, content, author) VALUES ('sport article', 'I am an article and this is the content', 'Bob');
+INSERT INTO article (name, content, author) VALUES ('news article', 'I am an article and this is the content', 'Bob');
+INSERT INTO article (name, content, author) VALUES ('people article', 'I am an article and this is the content', 'Alice');
+INSERT INTO article (name, content, author) VALUES ('science article', 'I am an article and this is the content', 'Alice');
