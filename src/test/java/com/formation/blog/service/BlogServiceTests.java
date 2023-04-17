5e3dc3ef3c334a652300d2309a61fccf37b37fe7
Son-tony DINH <sodin@P-LYO-URFE.dhcp.lyon.intranet>
2023-04-17 15:01:17 +0200

    polish, com.formation.blog.system removed because it was useless

--- src/test/java/com/formation/blog/service/BlogServiceTests.java
+++ src/test/java/com/formation/blog/service/BlogServiceTests.java
@@ -1,6 +1,7 @@
 package com.formation.blog.service;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -11,8 +12,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.formation.blog.web.ArticleController;
 
-//@RunWith(SpringRunner.class)
-//@WebMvcTest(ArticleController.class)
 @WebMvcTest(controllers = ArticleController.class)
 public class BlogServiceTests {
 	
@@ -27,29 +26,11 @@ public class BlogServiceTests {
         mockMvc.perform(get("/articles"))
             .andExpect(status().isOk());
     }
-	
-//	@Autowired
-//    private BlogService blogService;
-//	
-//	@Test
-//    void shouldFindArticlesByAuthor() {
-//        Collection<Article> articles = this.blogService.findArticleByAuthor("Bob");
-//        assertThat(articles.size()).isEqualTo(2);
-//
-//        articles = this.blogService.findArticleByAuthor("Bobb");
-//        assertThat(articles.isEmpty()).isTrue();
-//    }
-	
-//	@Autowired
-//    private BlogService blogService;
-//
-//    @Test
-//    void shouldFindOwnersByLastName() {
-//        Collection<Article> articles = this.blogService.findArticleByAuthor("Bob");
-//        assertThat(articles.size()).isEqualTo(2);
-//
-//        articles = this.blogService.findArticleByAuthor("Bobb");
-//        assertThat(articles.isEmpty()).isTrue();
-//    }
+    
+    @Test
+    public void testCreateArticles() throws Exception {
+    	mockMvc.perform(post("/articles/new"))
+        .andExpect(status().isOk());
+    }
 
 }
