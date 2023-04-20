0beb4c015f699270f27c94b6b20b685d20a7cb8b
Son-tony DINH <sodin@P-LYO-URFE.dhcp.lyon.intranet>
2023-04-20 13:59:50 +0200

    ArticleController: change the return type of posRequest createArticle to return a response entity with httpStatusCreated

--- src/main/java/com/formation/blog/web/ArticleController.java
+++ src/main/java/com/formation/blog/web/ArticleController.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -51,8 +53,9 @@ public class ArticleController {
 	}
 	
 	@PostMapping("/articles/new")
-	public void createArticle(@RequestBody Article article) {
+	public ResponseEntity<Article> createArticle(@RequestBody Article article) {
 		articleService.createArticle(article);
+		return new ResponseEntity<>(article, HttpStatus.CREATED);
 	}
 	
 	@DeleteMapping("/articles/{articleId}")
