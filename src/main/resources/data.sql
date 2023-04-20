149e896811eafbb9f62669da17b35d4d6b7477c1
Son-tony DINH <sodin@P-LYO-URFE.dhcp.lyon.intranet>
2023-04-20 09:59:26 +0200

    test delete parent object

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+
 INSERT INTO article (name, content, author) VALUES ('article name', 'I am an article and this is the content', 'John');
 INSERT INTO article (name, content, author) VALUES ('car article', 'I am an article and this is the content', 'John');
 INSERT INTO article (name, content, author) VALUES ('plane article', 'I am an article and this is the content', 'John');
@@ -5,7 +6,6 @@ INSERT INTO article (name, content, author) VALUES ('sport article', 'I am an ar
 INSERT INTO article (name, content, author) VALUES ('news article', 'I am an article and this is the content', 'Bob');
 INSERT INTO article (name, content, author) VALUES ('people article', 'I am an article and this is the content', 'Alice');
 INSERT INTO article (name, content, author) VALUES ('science article', 'I am an article and this is the content', 'Alice');
-
 
 INSERT INTO review (content, author, article_id) VALUES ('Good articles !', 'David', 3);
 INSERT INTO review (content, author, article_id) VALUES ('Very good articles !', 'Henry', 5);
